3872ce22310243f143be7362f003d404ed051669
GiAnG <hoang.giang.do.tien@autodesk.com>
2024-11-25 01:07:41 +0800

    init

---
+++ s3-be/src/main/java/com/example/s3be/S3BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3BeApplication.class, args);
+	}
+
+}
---
+++ s3-be/src/test/java/com/example/s3be/S3BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s3be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
